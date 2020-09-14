498f4248fccdeb50cfd2d1cd2c3ff3a550132fe7
Tomas Vymazal <tomas.vymazal@gmail.com>
2020-09-14 19:45:52 +0200

    WIP

---
+++ Benchmark.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Diagnostics;
+
+// ReSharper disable once CheckNamespace
+namespace TweakAsSharp
+{
+    public static class Benchmark
+    {
+        public static void RunEvalBenchmark(int iterationCount = 100)
+        {
+            float sum = 0, min = float.MaxValue, max = float.MinValue;
+            
+            for (int i = 0; i < iterationCount; i++)
+            {
+                var timer = new Stopwatch();
+                timer.Start();
+
+                var value = TweakAS.Evaluate(""+i);    // hope that optimizer won't take this away
+                
+                timer.Stop();
+
+                var elapsed = timer.ElapsedMilliseconds;
+
+                sum += elapsed;
+
+                if (elapsed < min)
+                    min = elapsed;
+
+                if (elapsed > max)
+                    max = elapsed;
+            }
+            
+            Console.WriteLine($"Code Eval runtime: Min: {min}, Max: {max}, Mean: {sum/iterationCount}");
+        }
+    }
+}
--- TweakAS.cs
+++ TweakAS.cs
@@ -3,40 +3,43 @@ using System.Diagnostics;
 using Microsoft.CodeAnalysis.CSharp.Scripting;
 using Microsoft.CodeAnalysis.Scripting;
 
-static class TweakAS
+namespace TweakAsSharp
 {
-    public static Tuple<string, int> GetFileLineInfo(int frameIndex = 1)
+    static class TweakAS
     {
-        var st = new StackTrace(true);
-
-        var frame = st.GetFrame(frameIndex);
-
-        if (frame != null)
+        public static Tuple<string, int> GetFileLineInfo(int frameIndex = 1)
         {
-            var filename = frame.GetFileName();
-            var line = frame.GetFileLineNumber();
+            var st = new StackTrace(true);
+
+            var frame = st.GetFrame(frameIndex);
+
+            if (frame != null)
+            {
+                var filename = frame.GetFileName();
+                var line = frame.GetFileLineNumber();
         
-            return new Tuple<string, int>(filename, line);
-        }
-        else
-        {
+                return new Tuple<string, int>(filename, line);
+            }
+
             return null;
         }
-    }
 
-    static TweakAS()
-    {
-        Console.Write("Warming up the runtime...");
-        Evaluate("42");    // warm up
-        Console.WriteLine(" Done");
-    }
+        static TweakAS()
+        {
+            Console.Write("Warming up the runtime...");
+            Evaluate("42");    // warm up
+            Console.WriteLine(" Done");
+        }
     
-    // not thread safe I guess
-    private static Script<object> _mBaseScript = CSharpScript.Create("");
+        // not thread safe I guess
+        private static Script<object> _mBaseScript = CSharpScript.Create("");
 
-    // not thread safe I guess
-    public static object Evaluate(string code)
-    {
-        return _mBaseScript.ContinueWith(code).RunAsync().Result.ReturnValue;
+        // not thread safe I guess
+        public static object Evaluate(string code)
+        {
+            return _mBaseScript.ContinueWith(code).RunAsync().Result.ReturnValue;
+        }
     }
 }
+
+// ReSharper disable once CheckNamespace
