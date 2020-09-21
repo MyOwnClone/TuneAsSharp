04633e1e1906613ecd81698e1ed3d0950e93488c
Tomas Vymazal <tomas.vymazal@gmail.com>
2020-09-21 15:41:02 +0200

    WIP

---
+++ TweakableFunctions/TweakableFunctions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TuneAsSharp;
+
+namespace TweakableFunctions
+{
+    class Program
+    {
+        [TweakableFunction]
+        private static int test()
+        {
+            return 0;
+        }
+        
+        private static object tf(Func<object> action)    /* Action<> is not able to return value, Func<> is  */
+        {
+            /*
+             * the plan is to trigger Parser here, reload __FILE__, look for all [TweakableFunction] functions,
+             * load them into the Roslyn and dispatch call to the roslyn context
+             * if we are able to call action within the context of code compiled from roslyn, the call to test()
+             * should resolve to the one parsed from the reloaded __FILE__.
+             * We should also check whether the test() was already parsed from __FILE__, if not, call the action()
+             * in original context without roslyn
+             * TODO: read https://laurentkempe.com/2019/02/18/dynamically-compile-and-run-code-using-dotNET-Core-3.0/
+             */
+            
+            
+            return action();
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine(tf(() => test()));
+            }
+        }
+    }
+}
---
+++ TweakableValues/TweakableValues.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq.Expressions;
+using TuneAsSharp;
+
+// ReSharper disable once CheckNamespace
+namespace TweakableValues
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            //RoslynBenchmark.Run();
+
+            /*Parser.UseRoslyn =
+                false; // simpler literals can be parsed without Roslyn, we'll need it only for actual code (tweakable functions)
+
+            var counter = 0;
+
+            while (true)
+            {
+                Console.WriteLine(counter + " " + TweakAS.tv("test") + " " + TweakAS.tv("moarolka"));
+
+                //Console.WriteLine( counter + " " + TweakAS.tv("gloryhole")); 
+                //Console.WriteLine( counter + " " + TweakAS.tv("testxyz"));
+
+                Console.WriteLine(OtherModule.MethodWithTweakableValue());
+
+                counter++;
+            }*/
+
+            Console.WriteLine(UnitTestRunner.Run(false));
+            Console.WriteLine(UnitTestRunner.Run(true));
+        }
+    }
+}
